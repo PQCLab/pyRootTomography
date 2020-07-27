741708f2804701ae75ca639d3a54822b9d904264
Boris Bantysh <bbantysh60000@gmail.com>
2020-07-28 01:44:47 +0300

    Added Example

---
+++ example.py
@@ -0,0 +1,13 @@
+import root_tomography.tools as tools
+from root_tomography.reconstruction import dm_reconstruct
+
+N = 1  # Number of qubits
+n = 1e3  # Number of repetitions in every experiment
+proto = tools.protocol("mub", [2]*N)
+nshots = tools.nshots_devide(n, len(proto), "equal")
+
+dm_true = tools.gen_state("haar_dm", 2, rank=1)  # Generate Haar-random single-qubit pure state density matrix
+clicks = tools.simulate(dm_true, proto, nshots)  # Simulate measurements
+
+dm, _ = dm_reconstruct(clicks, proto, nshots, display=True)
+print("Fidelity: {}".format(tools.fidelity(dm, dm_true)))
